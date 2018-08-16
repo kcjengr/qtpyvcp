ceb9583d47dfc7c64ba7d8c2b28704ba59a9c393
Kurt Jacobson <kcjengr@gmail.com>
2018-08-16 14:34:15 -0400

    widgets: add support for numeric values in status label

--- QtPyVCP/widgets/display_widgets/statuslabel_widget.py
+++ QtPyVCP/widgets/display_widgets/statuslabel_widget.py
@@ -12,16 +12,63 @@ INFO = Info()
 
 from QtPyVCP.enums import Axis, ReferenceType, Units
 
-SIG_NAMES = {
-    0: "motion_type",
-    1: "task_state",
-    2: "motion_mode",
-    3: "interp_state",
-    4: "g5x_index",
-    5: "program_units",
-    6: "gcodes",
-    7: "mcodes",
-    8: "interpreter_errcode",
+MAPPING = {
+    0: {'name': 'motion_type',
+        'format': '',
+        'tooltip': 'Motion Type',
+        },
+    1: {'name': 'task_state',
+        'format': '',
+        'tooltip': 'Task State',
+        },
+    2: {'name': 'motion_mode',
+        'format': '',
+        'tooltip': 'Motion Mode',
+        },
+    3: {'name': 'interp_state',
+        'format': '',
+        'tooltip': 'Interp State',
+        },
+    4: {'name': 'g5x_index',
+        'format': '',
+        'tooltip': 'Active Work System',
+        },
+    5: {'name': 'program_units',
+        'format': '',
+        'tooltip': 'Active Unit System',
+        },
+    6: {'name': 'gcodes',
+        'format': '',
+        'tooltip': 'Active G-codes',
+        },
+    7: {'name': 'mcodes',
+        'format': '',
+        'tooltip': 'Active M-codes',
+        },
+    8: {'name': 'interpreter_errcode',
+        'format': '',
+        'tooltip': 'Interp Error',
+        },
+    9: {'name': 'file',
+        'format': '',
+        'tooltip': 'Loaded NGC File',
+        },
+    10: {'name': 'feedrate',
+        'format': '{:.0%}',
+        'tooltip': 'Feed Override',
+        },
+    11: {'name': 'spindlerate',
+        'format': '{:.0%}',
+        'tooltip': 'Speed Override',
+        },
+    12: {'name': 'rapidrate',
+        'format': '{:.0%}',
+        'tooltip': 'Rapid Override',
+        },
+    13: {'name': 'max_velocity',
+        'format': '{:.2f}',
+        'tooltip': 'Max Velocity',
+        },
 }
 
 class LabelType(object):
@@ -34,6 +81,11 @@ class LabelType(object):
     gcodes = 6
     mcodes = 7
     interpreter_errcode = 8
+    file = 9
+    feed_override = 10
+    speed_override = 11
+    rapid_override = 12
+    max_velocity = 13
 
 class StatusLabel(QLabel, LabelType):
 
@@ -43,16 +95,26 @@ class StatusLabel(QLabel, LabelType):
         super(StatusLabel, self).__init__(parent)
 
         self._label_type = 1
-        self.setText(SIG_NAMES[self._label_type])
+        self.setText(MAPPING[self._label_type]['name'])
 
         STATUS.init_ui.connect(self.initUI)
 
     def initUI(self):
-        sig_name = SIG_NAMES[self._label_type]
+        sig_name = MAPPING[self._label_type]['name']
+        format_spec = MAPPING[self._label_type]['format']
+        tooltip_text = MAPPING[self._label_type]['tooltip']
+
         value = getattr(STATUS.stat, sig_name)
-        value_str = STATUS.STATE_STRING_LOOKUP.get(sig_name)[value]
-        getattr(STATUS, sig_name)[str].connect(self.update)
-        self.setText(value_str)
+        try:
+            value = STATUS.STATE_STRING_LOOKUP[sig_name][value]
+            getattr(STATUS, sig_name)[str].connect(self.update)
+        except KeyError:
+            if isinstance(value, str) and format_spec == '':
+                getattr(STATUS, sig_name).connect(self.update)
+            else:
+                getattr(STATUS, sig_name).connect(lambda v: self.update(format_spec.format(v)))
+                value = format_spec.format(value)
+        self.setText(value)
 
     def update(self, status_str):
         self.setText(status_str)
@@ -61,8 +123,8 @@ class StatusLabel(QLabel, LabelType):
         return self._label_type
     @pyqtSlot(LabelType)
     def setLabelType(self, label_type):
-        sig_name = SIG_NAMES[label_type]
-        self.setText(sig_name)
-        self.setToolTip(sig_name)
+        tooltip = MAPPING[label_type]['tooltip']
+        self.setText(tooltip)
+        self.setToolTip(tooltip)
         self._label_type = label_type
     label_type = pyqtProperty(LabelType, getLabelType, setLabelType)
