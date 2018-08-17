c0e21c13ddb25a6e95a9ed8717090b333ff70b5b
Lcvette <small.shop.concepts@gmail.com>
2018-08-17 10:51:46 -0400

    changed load meter appearance and spindle slider but hazzy says wrong

--- QtPyVCP/widgets/display_widgets/load_meter.py
+++ QtPyVCP/widgets/display_widgets/load_meter.py
@@ -13,34 +13,34 @@ class LoadMeter(QProgressBar):
 
         val = self.value()
         pos = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), val, self.width())
-        pos60 = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), 90, self.width())
-        pos80 = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), 110, self.width())
+        pos60 = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), 100, self.width())
+        pos80 = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), 120, self.width())
 
         p = QPainter(self)
-        p.setPen(Qt.green)
-        p.setBrush(QBrush(Qt.green))
+        p.setPen(Qt.blue)
+        p.setBrush(QBrush(Qt.blue))
 
-        if val >= 0 and val <= 90:
+        if val >= 0 and val <= 100:
             p.drawRect(0, 0, pos, self.height())
 
-        elif val > 90 and val <= 110:
+        elif val > 90 and val <= 120:
 
             p.drawRect(0,0,pos60, self.height())
-            p.setPen(QColor(255, 127, 0))
-            p.setBrush(QBrush(QColor(255, 127, 0)))
+            p.setPen(Qt.yellow)
+            p.setBrush(QBrush(Qt.yellow))
             p.drawRect(pos60, 0, pos - pos60, self.height())
 
         else:
             p.drawRect(0,0,pos60, self.height())
-            p.setPen(QColor(255, 127, 0))
-            p.setBrush(QBrush(QColor(255, 127, 0)))
+            p.setPen(Qt.yellow)
+            p.setBrush(QBrush(Qt.yellow))
             p.drawRect(pos60, 0, pos80 - pos60, self.height())
             p.setPen(Qt.red)
             p.setBrush(QBrush(Qt.red))
             p.drawRect(pos80, 0, pos - pos80, self.height())
 
-        p.setPen(Qt.lightGray)
-        p.setBrush(QBrush(Qt.lightGray))
+        p.setPen(QColor(235, 235, 238))
+        p.setBrush(QBrush(QColor(235, 235, 238)))
         p.drawRect(pos, 0, self.width(), self.height())
 
 
