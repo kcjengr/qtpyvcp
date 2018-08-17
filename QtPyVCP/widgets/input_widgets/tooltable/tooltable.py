a30ed020e88aaf65680c8e0c09c628f5dc71b155
TurBoss <j.l.toledano.l@gmail.com>
2018-08-17 05:22:13 +0200

    load the tooltable

--- QtPyVCP/widgets/input_widgets/tooltable/tooltable.py
+++ QtPyVCP/widgets/input_widgets/tooltable/tooltable.py
@@ -26,15 +26,13 @@ import sys
 
 import linuxcnc
 
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
-
-from PyQt5.QtCore import QByteArray, Qt, QTimer
-from PyQt5.QtGui import QPalette, QPixmap
-from PyQt5.QtWidgets import (QAction, QActionGroup, QApplication,
-                             QWidget, QMessageBox)
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QCheckBox
 
 # Set up logging
 from QtPyVCP.utilities import logger
+from QtPyVCP.utilities.info import Info
 
 LOG = logger.getLogger(__name__)
 
@@ -46,66 +44,86 @@ class ToolTable(QWidget):
     def __init__(self, parent=None):
         super(ToolTable, self).__init__(parent)
 
+        info = Info()
+
         self.log = LOG
 
         self.ui = uic.loadUi(os.path.join(WIDGET_PATH, "tooltable.ui"), self)
 
+        self.tool_table_file = info.getToolTableFile()
+        self.load_tool_table()
+
+        self.toolinfo = []
+
+        self.ui.load_button.clicked.connect(self.load_tool_table)
 
     # Parse and load tool table into the treeview
     # More or less copied from Chris Morley's GladeVcp tooledit widget
 
-    def load_tool_table(self, fn=None):
-        # If no valid tool table given
-        if fn is None:
-            fn = self.tool_table_file
+    def load_tool_table(self):
+
+        self.ui.tooltable.clear()
+
+
+        fn = self.tool_table_file
+
         if not os.path.exists(fn):
             self.log.warning("Tool table does not exist")
             return
-        self.model.clear()  # Clear any existing data
+
         self.log.debug("Loading tool table: {0}".format(fn))
+
         with open(fn, "r") as tf:
             tool_table = tf.readlines()
 
-        self.toolinfo = []  # TODO move to __init__
-        for line in tool_table:
+        for count, line in enumerate(tool_table):
+
             # Separate tool data from comments
             comment = ''
             index = line.find(";")  # Find comment start index
+
             if index == -1:  # Delimiter ';' is missing, so no comments
                 line = line.rstrip("\n")
             else:
                 comment = (line[index + 1:]).rstrip("\n")
                 line = line[0:index].rstrip()
+
             array = [False, 1, 1, '0', '0', comment, None]
+
             # search beginning of each word for keyword letters
             # offset 0 is the checkbox so ignore it
             # if i = ';' that is the comment and we have already added it
             # offset 1 and 2 are integers the rest floats
+
             for offset, i in enumerate(['S', 'T', 'P', 'D', 'Z', ';']):
                 if offset == 0 or i == ';':
                     continue
+
                 for word in line.split():
                     if word.startswith(i):
                         if offset in (1, 2):
                             try:
                                 array[offset] = int(word.lstrip(i))
+                                for i in range(len(array)):
+                                    self.ui.tooltable.setItem(count, i, QTableWidgetItem(self.handleItem(array[i])))
+
                             except ValueError:
                                 msg = 'Error reading tool table, can\'t convert "{0}" to integer in {1}' \
                                     .format(word.lstrip(i), line)
                                 self.log.error(msg)
-                                self.widget_window.show_error(msg)
+                                # self.widget_window.show_error(msg)
                         else:
                             try:
                                 array[offset] = "%.4f" % float(word.lstrip(i))
+                                for i in range(len(array)):
+                                    self.ui.tooltable.setItem(count, i, QTableWidgetItem(self.handleItem(array[i])))
+
                             except ValueError:
                                 msg = 'Error reading tool table, can\'t convert "{0}" to float in {1}' \
                                     .format(word.lstrip(i), line)
                                 self.log.error(msg)
-                                self.widget_window.show_error(msg)
+                                # self.widget_window.show_error(msg)
                         break
-
-            # Add array to liststore
-            self.model.append(array)
 
     # Save tool table
     # More or less copied from Chris Morley's GladeVcp tooledit widget
@@ -134,3 +152,15 @@ class ToolTable(QWidget):
         fn.flush()
         os.fsync(fn.fileno())
         linuxcnc.command().load_tool_table()
+
+    def handleItem(self, item):
+
+        if isinstance(item,bool):
+            if item:
+                return "yes"
+            else:
+                return "no"
+        elif isinstance(item, str):
+            return item
+        elif item is None:
+            return ""
