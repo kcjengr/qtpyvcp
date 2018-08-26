dfa85b68c0c4000b2f83d80d1c09e1212331aedf
Kurt Jacobson <kcjengr@gmail.com>
2018-08-25 23:47:40 -0400

    add basic performance monitoring

--- QtPyVCP/application.py
+++ QtPyVCP/application.py
@@ -6,6 +6,7 @@ Contains the VCPApplication class with core function and VCP loading logic.
 import os
 import sys
 
+from PyQt5.QtCore import QTimer, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, qApp
 
 class VCPApplication(QApplication):
@@ -28,19 +29,37 @@ class VCPApplication(QApplication):
 
         ui_file = None
         if vcp is not None and os.path.exists(vcp):
-            if os.path.isfile(vcp):
-                print "VCP does exist"
-                directory, filename = os.path.split(vcp)
+            vcp_path = os.path.realpath(vcp)
+            if os.path.isfile(vcp_path):
+                directory, filename = os.path.split(vcp_path)
                 name, ext = os.path.splitext(filename)
                 if ext == '.ui':
                     print "is a UI file"
-                    ui_file = vcp
+                    window = VCPMainWindow(ui_file=vcp_path)
+                    window.show()
+                    self.window = window
                 elif ext == '.py':
                     print "is a python file"
-            elif os.path.isdir(vcp):
+            elif os.path.isdir(vcp_path):
                 print "VCP is a directory"
 
+        # Performance monitoring
+        if perfmon:
+            import psutil
+            self.perf = psutil.Process()
+            self.perf_timer = QTimer()
+            self.perf_timer.setInterval(2000)
+            self.perf_timer.timeout.connect(self.logPerformance)
+            self.perf_timer.start()
 
-        window = VCPMainWindow(ui_file=ui_file)
-        window.show()
-        self.window = window
+
+    @pyqtSlot()
+    def logPerformance(self):
+        """
+        Logs total CPU usage (in percent), as well as per-thread usage.
+        """
+        with self.perf.oneshot():
+            total_percent = self.perf.cpu_percent(interval=None)
+            total_time = sum(self.perf.cpu_times())
+            usage = [total_percent * ((t.system_time + t.user_time) / total_time) for t in self.perf.threads()]
+        print("Total: {tot}, Per Thread: {percpu}".format(tot=total_percent, percpu=usage))
