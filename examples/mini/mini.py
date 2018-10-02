8f7862c5359830281a1b0a6ec014ea65a25fe784
Kurt Jacobson <kcjengr@gmail.com>
2018-10-02 10:21:19 -0400

    examples: clean up mini

--- examples/mini/mini.py
+++ examples/mini/mini.py
@@ -3,6 +3,8 @@ from QtPyVCP.widgets.form_widgets.main_window import VCPMainWindow
 
 VCP_DIR = os.path.abspath(os.path.dirname(__file__))
 
+__version__ = '0.0.1'
+
 class MiniVCP(VCPMainWindow):
     """Main window class for the Mini VCP."""
     def __init__(self, *args, **kwargs):
@@ -12,11 +14,14 @@ class MiniVCP(VCPMainWindow):
         self.loadUi(ui_file) # load the UI file
         self.initUi()        # initialize menu actions etc.
 
+# Main entry point
 def main():
     import sys
+    from QtPyVCP.utilities.opt_parser import parse_opts
+    opts = parse_opts(vcp_name='Mini', vcp_version=__version__)
     from QtPyVCP.application import VCPApplication
-    app = VCPApplication(ini=sys.argv[1])
-    app.window = MiniVCP()
+    app = VCPApplication(opts=opts)
+    app.window = MiniVCP(opts=opts)
     app.window.show()
     sys.exit(app.exec_())
 
