eec04ff997912ca86a297d193bb47202edda1cb1
Kurt Jacobson <kcjengr@gmail.com>
2018-10-02 10:25:42 -0400

    setup: add console script for mini

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'qtpyvcp=vcp_launcher.vcp_launcher:main',
+            'mini=examples.mini.mini:main'
         ],
         'qtpyvcp.example_vcp': [
             'mini=examples.mini.mini:MiniVCP',
