24407cca262d7e56c437d4b716b0e5e836a0009b
Kurt Jacobson <kcjengr@gmail.com>
2018-10-04 14:59:45 -0400

    load_meter: make load meter style customizable in designer

--- QtPyVCP/widgets/display_widgets/load_meter.py
+++ QtPyVCP/widgets/display_widgets/load_meter.py
@@ -1,63 +1,135 @@
 #!/usr/bin/python
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt, pyqtProperty, QRectF
+from PyQt5.QtGui import QColor, QLinearGradient, QPainter, QPen
+from PyQt5.QtWidgets import QProgressBar, QStyle
 
-import time
+from QtPyVCP.utilities import logger
+LOG = logger.getLogger(__name__)
 
 class LoadMeter(QProgressBar):
     """docstring for LoadMeter"""
     def __init__(self, parent=None):
         super(LoadMeter, self).__init__(parent)
 
-        self.gradient = self.makeGradient()
+        self.barGradient = [u'0.0, 0, 255, 0',
+                            u'0.8, 255, 255, 0',
+                            u'1.0, 255, 0, 0',]
 
-    def makeGradient(self):
-        g = QLinearGradient(0, 0, self.width(), self.height())
-        # g.setColorAt(0, Qt.green)
-        # g.setColorAt(.8, Qt.yellow)
-        # g.setColorAt(1, Qt.red)
-
-        g.setColorAt(0, QColor(170, 170, 236))
-        g.setColorAt(0.63, QColor(85, 85, 238))
-
-        g.setColorAt(0.65, QColor(171, 177, 158))
-        g.setColorAt(0.79, QColor(227, 237, 106))
-        
-        g.setColorAt(0.84, QColor(219, 124, 55))
-        g.setColorAt(1, QColor(209, 0, 0))
-
-
-        return g
+        self._text_color = QColor(0, 0, 0)
+        self._border_color = Qt.gray
+        self._border_radius = 2
+        self._border_width = 1
 
     def paintEvent(self, event):
 
+        bw = float(self._border_width)
+        br = self._border_radius
+
         val = self.value()
-        pos = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), val, self.width())
+        if self.orientation() == Qt.Horizontal:
+            w = QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), val, self.width())
+            h = self.height()
+            rect = QRectF(bw / 2, bw / 2, w - bw, h - bw)
+        else:
+            w = self.width()
+            h = self.height() - QStyle.sliderPositionFromValue(self.minimum(), self.maximum(), val, self.height())
+            rect = QRectF(bw / 2, h - bw / 2, w - bw, self.height() - bw)
 
         p = QPainter(self)
         p.setRenderHint(QPainter.Antialiasing);
 
         # draw the load meter value bar
         p.setPen(Qt.transparent)
-        self.gradient.setFinalStop(self.width(), self.height())
         p.setBrush(self.gradient)
-        rect = QRectF(.5, .5, pos - 1, self.height() - 1)
-        p.drawRoundedRect(rect, 2.0, 2.0)
+        p.drawRoundedRect(rect, br, br)
 
         # draw the border
         p.setBrush(Qt.transparent)
         border_pen = QPen()
-        border_pen.setWidth(1)
-        border_pen.setColor(Qt.gray)
+        border_pen.setWidth(bw)
+        border_pen.setColor(self._border_color)
         p.setPen(border_pen)
-        rect = QRectF(.5, .5, self.width() - 1, self.height() - 1)
-        p.drawRoundedRect(rect, 2.0, 2.0)
+        rect = QRectF(bw / 2, bw / 2, self.width() - bw, self.height() - bw)
+        p.drawRoundedRect(rect, br, br)
 
         # draw the load percentage text
-        p.setPen(QColor(235, 235, 236))
-        p.drawText(0,0, self.width(), self.height(), Qt.AlignCenter, str(val) + "%")
+        p.setPen(self._text_color)
+        if self.orientation() == Qt.Vertical:
+            p.rotate(-90)
+            p.drawText(-self.height(), 0, self.height(), self.width(), Qt.AlignCenter, self.text())
+        else:
+            p.drawText(0,0, self.width(), self.height(), Qt.AlignCenter, self.text())
+
+    def resizeEvent(self, event):
+        if self.orientation() == Qt.Horizontal:
+            self.gradient.setStart(0, 0)
+            self.gradient.setFinalStop(self.width(), 0)
+        else:
+            self.gradient.setStart(0, self.height())
+            self.gradient.setFinalStop(0, 0)
+
+    # border width
+    @pyqtProperty('QStringList')
+    def barGradient(self):
+        return self._gradient_def
+
+    @barGradient.setter
+    def barGradient(self, gradient):
+        grad = QLinearGradient(0, 0, 0, 0)
+        try:
+            for stop in gradient:
+                pos, r, g, b = stop.split(',')[:4]
+                color = QColor(int(r), int(g), int(b))
+                grad.setColorAt(float(pos), color)
+        except:
+            LOG.exception('Invalid gradient.')
+            return
+
+        self._gradient_def = gradient
+        self.gradient = grad
+        self.resizeEvent(None)
+        self.update()
+
+    # text color
+    @pyqtProperty(QColor)
+    def textColor(self):
+        return self._text_color
+
+    @textColor.setter
+    def setTextColor(self, text_color):
+        self._text_color = text_color
+        self.update()
+
+    # border color
+    @pyqtProperty(QColor)
+    def borderColor(self):
+        return self._border_color
+
+    @borderColor.setter
+    def borderColor(self, border_color):
+        self._border_color = border_color
+        self.update()
+
+    # border radius
+    @pyqtProperty(int)
+    def borderRadius(self):
+        return self._border_radius
+
+    @borderRadius.setter
+    def borderRadius(self, border_radius):
+        self._border_radius = border_radius
+        self.update()
+
+    # border width
+    @pyqtProperty(int)
+    def borderWidth(self):
+        return self._border_width
+
+    @borderWidth.setter
+    def borderWidth(self, border_width):
+        self._border_width = border_width
+        self.update()
 
 if __name__ == "__main__":
     import sys
