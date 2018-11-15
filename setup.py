acf3d7963166aae2f63d167179af64d176a9305a
Kurt Jacobson <kcjengr@gmail.com>
2018-11-15 13:33:40 -0500

    DIST: install sim files in configs dir

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
+import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 from qtpyvcp.tools.qcompile import compile
-
 compile(['examples/probe_basic',])
+
+data_dirs = [
+('~/linuxcnc/configs/sim.qtpyvcp', 'sim'),
+# ('~/linuxcnc/vcps', 'examples'),
+]
+
+def data_files_from_dirs(data_dirs):
+    data_files = []
+    for dest_dir, source_dir in data_dirs:
+        dest_dir = os.path.expanduser(dest_dir)
+        for root, dirs, files in os.walk(source_dir):
+            root_files = [os.path.join(root, i) for i in files]
+            dest = os.path.join(dest_dir, os.path.relpath(root, source_dir))
+            print dest
+            data_files.append((dest, root_files))
+    return data_files
 
 setup(
     name="qtpyvcp",
@@ -18,6 +34,7 @@ setup(
     url="https://gitlab.com/Hazzy/qtpyvcp",
     download_url="https://gitlab.com/Hazzy/qtpyvcp/-/archive/master/qtpyvcp-master.tar.gz",
     packages=find_packages(),
+    data_files=data_files_from_dirs(data_dirs),
     include_package_data=True,
     install_requires=[
     'docopt',
