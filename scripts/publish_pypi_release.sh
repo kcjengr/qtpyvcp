714975627f54e4a8fc4c805bd6ea3dcd55e56bbb
Kurt Jacobson <kcjengr@gmail.com>
2019-01-09 17:51:56 -0500

    BLD: don't add comment when uploading to PyPi
    This is an attempt to fix the description formatting on PyPi.

--- scripts/publish_pypi_release.sh
+++ scripts/publish_pypi_release.sh
@@ -7,5 +7,4 @@ echo 'Uploading files to PyPi...'
 twine upload \
     --username kcjengr \
     --password $PYPI_PASS \
-    --comment "This is a test comment for the QtPyVCP $TRAVIS_TAG release." \
     'dist/qtpyvcp*.tar.gz'
