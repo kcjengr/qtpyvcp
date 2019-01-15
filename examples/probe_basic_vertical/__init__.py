83ef30d5a0a7c46e902d0e7c8fb89bc833119c91
Kurt Jacobson <kcjengr@gmail.com>
2019-01-15 11:43:45 -0500

    VCP: Add Probe Basic Vertical
    pb vertical updates
    
    
    update

---
+++ examples/probe_basic_vertical/__init__.py
@@ -0,0 +1,23 @@
+
+__version__ = '0.0.1'
+
+import os
+import qtpyvcp
+
+VCP_DIR = os.path.realpath(os.path.dirname(__file__))
+VCP_CONFIG_FILE = os.path.join(VCP_DIR, 'probe_basic.yml')
+
+
+def main(opts=None):
+
+    if opts is None:
+        from qtpyvcp.utilities.opt_parser import parse_opts
+        opts = parse_opts(vcp_cmd='probebasic',
+                          vcp_name='Probe Basic',
+                          vcp_version=__version__)
+
+    qtpyvcp.run_vcp(opts, VCP_CONFIG_FILE)
+
+
+if __name__ == '__main__':
+    main()
