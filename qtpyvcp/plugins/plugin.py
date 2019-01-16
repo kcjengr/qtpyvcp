20e3cdb2fed7da63799bb4232d1e85f86e563d38
Kurt Jacobson <kcjengr@gmail.com>
2019-01-16 15:52:24 -0500

    new DataChannel implementation

--- qtpyvcp/plugins/plugin.py
+++ qtpyvcp/plugins/plugin.py
@@ -3,13 +3,14 @@ from qtpyvcp.utilities.logger import getLogger
 
 LOG = getLogger(__name__)
 
-class QtPyVCPDataPlugin(QObject):
+class DataPlugin(QObject):
     """QtPyVCPDataPlugin."""
 
-    protocol = None
-
     def __init__(self):
-        super(QtPyVCPDataPlugin, self).__init__()
+        super(DataPlugin, self).__init__()
+
+        self.channels = {chan: getattr(self, chan) for chan in dir(self) if
+                         isinstance(getattr(self, chan), DataChannel)}
 
     def initialise(self):
         """Initialize the data plugin.
@@ -23,63 +24,93 @@ class QtPyVCPDataPlugin(QObject):
         """Terminate the data plugin.
 
         This is called right before the main event loop exits. Any cleanup
-        of the plugin should be done here, such as saving data to a file.
+        of the plugin should be done here, such as saving persistent data.
         """
         pass
 
 
-class QtPyVCPDataChannel(QObject):
-    """QtPyVCPChannel.
+class DataChannel(QObject):
 
-    Args:
-        description (str) : A human readable description of the item.
-    """
+    _signal = Signal(object)
 
-    valueChanged = Signal(object)
+    def __init__(self, fget=None, fset=None, fstr=None, sig=None, doc=None):
+        super(DataChannel, self).__init__()
+        print 'fget:', fget
 
-    def __init__(self, description=''):
-        super(QtPyVCPDataChannel, self).__init__()
+        self.fget = fget
+        self.fset = fset
+        self.fstr = fstr
 
-        self.description = description
+        if sig is not None:
+            self._signal = sig
 
-    def handleQuery(self, query):
-        """Query channel value.
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
 
-        Args:
-            query (str) : The name of the value to query. If no query
-                is passed or it is an empty string the current value of
-                the ``value`` property will be returned.
+        self._data = None
 
-        Returns:
-            The queried value, or None.
-        """
-        try:
-            return getattr(self, query or 'value')
-        except AttributeError:
-            LOG.exception("Failed to handle query: {}".format(query))
-            return None
+    def getData(self, *args, **kwargs):
+        if self.fget is None:
+            return self._data
+        return self.fget(self, *args, **kwargs)
 
-    def handleAssigment(self, attr, value):
-        raise NotImplemented
-        # setattr(self, attr, value)
+    def getter(self, fget):
+        print 'fget:', fget
 
-    @property
-    def value(self):
-        """Channel value property.
+        def inner(*args, **kwargs):
+            fget(self, *args, **kwargs)
 
-        In a channel implementation the `value` property should return the
-        current value of the channel. If getting the value is reasonably
-        fast and light (e.g. dict lookup) then that can be done here, but if
-        it may take some time (e.g. over a network), then this method should
-        return a cashed value that is updated periodically.
-        """
-        pass
+        self.fget = inner
+        return self
+        return type(self)(fget=inner, fset=self.fset, fstr=self.fstr,
+                          sig=self._signal, doc=self.__doc__)
 
-    def onValueChanged(self, slot):
-        """Connect a callback to the valueChanged signal.
+    def setData(self, *args, **kwargs):
+        if self.fset is None:
+            if self.__doc__ is not None:
+                print self.__doc__.split('\n')[0].strip()
+            raise AttributeError('Channel is read only')
+        self.fset(self, *args, **kwargs)
 
-        Args:
-            slot : The method to call when the value changes.
-        """
-        self.valueChanged.connect(slot)
+    def setter(self, fset):
+        print 'fset:', fset
 
+        def inner(*args, **kwargs):
+            fset(*args, **kwargs)
+            self._signal.emit(self._data)
+
+        self.fset = inner
+        return self
+        return type(self)(fget=self.fget, fset=inner, fstr=self.fstr,
+                          sig=self._signal, doc=self.__doc__)
+
+    def __str__(self, *args, **kwargs):
+        if self.fstr is None:
+            print "fstr is none"
+            return str(self._data)
+        return self.fstr(self, *args, **kwargs)
+
+    def tostring(self, fstr):
+        print 'fstr:', fstr
+
+        def inner(*args, **kwargs):
+            return fstr(*args, **kwargs)
+
+        self.fstr = inner
+        return self
+        return type(self)(fget=self.fget, fset=self.fset, fstr=inner,
+                          sig=self._signal, doc=self.__doc__)
+
+    def notify(self, slot):
+        print 'Connecting %s to slot %s' % (self._signal, slot)
+        self._signal.connect(slot)
+
+    # fixme
+    onValueChanged = notify
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __call__(self, *args, **kwargs):
+        return self.getData(*args, **kwargs)
