26ae2aacc3ed3f0e0affce7975088cdc720c454e
Kurt Jacobson <kcjengr@gmail.com>
2019-01-18 01:41:15 -0500

    ToolTable - lathe tool orientation (Q) should be an int

--- qtpyvcp/plugins/tool_table.py
+++ qtpyvcp/plugins/tool_table.py
@@ -31,7 +31,7 @@ DEFAULT_TOOL = {
     'I': 0.0,
     'J': 0.0,
     'P': -1,
-    'Q': 0.0,
+    'Q':  0,
     'T': -1,
     'U': 0.0,
     'V': 0.0,
@@ -196,7 +196,7 @@ class ToolTable(QtPyVCPDataPlugin):
                 descriptor = item[0]
                 if descriptor in 'TPXYZABCUVWDIJQ':
                     value = item.lstrip(descriptor)
-                    if descriptor in ('T', 'P'):
+                    if descriptor in ('T', 'P', 'Q'):
                         try:
                             tool[descriptor] = int(value)
                         except:
