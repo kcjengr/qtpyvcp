a8bf2b478b5f720bd830c7ba91d813cd2e8ab179
Kurt Jacobson <kcjengr@gmail.com>
2019-01-22 21:46:04 -0500

    ENH: improve libpyqt5.so install script to can be run from any dir

--- QtDesigner/Qt5.7.1-Py2.7-64bit/install.sh
+++ QtDesigner/Qt5.7.1-Py2.7-64bit/install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-libpath="/usr/lib/x86_64-linux-gnu/qt5/plugins/designer"
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+LIB_PATH="/usr/lib/x86_64-linux-gnu/qt5/plugins/designer"
 
-oldlib=$libpath/"libpyqt5.so"
-if [ -f "$oldlib" ]
+OLD_LIB=$LIB_PATH/"libpyqt5.so"
+if [ -f "$OLD_LIB" ]
 then
     echo "renaming existing libpyqt5.so to libpyqt5.so.old"
-    mv "$oldlib" "$oldlib.old"
+    mv "$OLD_LIB" "$OLD_LIB.old"
 fi
 
-echo "copying libpyqt5_py2.so to $libpath"
-cp  $PWD/libpyqt5_py2.so $libpath
+echo "copying libpyqt5_py2.so to $LIB_PATH"
+cp  $SCRIPT_PATH/libpyqt5_py2.so $LIB_PATH
 
-echo "copying libpyqt5_py3.so to $libpath"
-cp  $PWD/libpyqt5_py3.so $libpath
+echo "copying libpyqt5_py3.so to $LIB_PATH"
+cp  $SCRIPT_PATH/libpyqt5_py3.so $LIB_PATH
