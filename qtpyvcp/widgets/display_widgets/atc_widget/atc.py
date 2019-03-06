a5ebfe343040e8d6c97efb1492459470f74b892d
TurBoss <j.l.toledano.l@gmail.com>
2019-03-06 22:28:14 +0100

    unused stuff

--- qtpyvcp/widgets/display_widgets/atc_widget/atc.py
+++ qtpyvcp/widgets/display_widgets/atc_widget/atc.py
@@ -9,13 +9,10 @@ ctypes.CDLL(ctypes.util.find_library("GL"), mode=ctypes.RTLD_GLOBAL)
 
 # end of Workarround
 
-from hal import component, HAL_BIT, HAL_IN
-
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, QUrl
 from PyQt5.QtQuickWidgets import QQuickWidget
 
 from qtpyvcp.plugins import getPlugin
-from qtpyvcp.widgets import VCPWidget
 from qtpyvcp.utilities import logger
 
 LOG = logger.getLogger(__name__)
@@ -38,12 +35,6 @@ class DynATC(QQuickWidget):
         url = QUrl.fromLocalFile(os.path.join(WIDGET_PATH, "atc.qml"))
         self.setSource(url)
 
-        self.fwd_pin = 0
-        self.rev_pin = 0
-
-        self.prev_fwd_pin = 0
-        self.prev_rev_pin = 0
-
         self.atc_position = 0
 
         STATUS.tool_in_spindle.notify(self.on_tool_in_spindle)
@@ -54,7 +45,6 @@ class DynATC(QQuickWidget):
 
     def on_tool_in_spindle(self, tool_num):
         print("Tool in Spindle: ", tool_num)
-
 
     rotateFwdSig = pyqtSignal(int, arguments=['rotate_forward'])
     @pyqtSlot()
@@ -67,21 +57,3 @@ class DynATC(QQuickWidget):
     def rotate_reverse(self):
         self.rotateRevSig.emit(self.atc_position)
         self.atc_position -= 1
-
-    pinSig = pyqtSignal(arguments=['get_pins'])
-    @pyqtSlot()
-    def get_pins(self):
-        if self.halcomp is None:
-            return
-
-        self.fwd_pin = self.halcomp["fwd"]
-        if self.fwd_pin != self.prev_fwd_pin:
-            self.prev_fwd_pin = self.fwd_pin
-            self.rotateFwdSig.emit(self.atc_position)
-            self.atc_position += 1
-
-        self.rev_pin = self.halcomp["rev"]
-        if self.rev_pin != self.prev_rev_pin:
-            self.prev_rev_pin = self.rev_pin
-            self.rotateRevSig.emit(self.atc_position)
-            self.atc_position += 1
