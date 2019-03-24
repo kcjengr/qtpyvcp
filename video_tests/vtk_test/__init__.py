91b8b6dfa0e8dfddb85b088af9ea24f36c49086e
John Thornton <jthornton@gnipsel.com>
2019-03-24 10:37:11 -0500

    ENH: add bare bones vcp's to test video cards

---
+++ video_tests/vtk_test/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Main entry point for vtk_test.
+
+This module contains the code necessary to be able to launch QControl
+directly from the command line, without using qtpyvcp. It handles
+parsing command line args and starting the main application.
+
+Example:
+    Assuming the dir this file is located in is on the PATH, you can
+    launch vtk_test by saying::
+
+        $ vtk_test --ini=/path/to/config.ini [options ...]
+
+    Run with the --help option to print a full list of options.
+
+"""
+
+__version__ = '0.0.1'
+
+import os
+import qtpyvcp
+
+VCP_DIR = os.path.realpath(os.path.dirname(__file__))
+VCP_CONFIG_FILE = os.path.join(VCP_DIR, 'config.yml')
+
+
+def main(opts=None):
+
+    if opts is None:
+        from qtpyvcp.utilities.opt_parser import parse_opts
+        opts = parse_opts(vcp_cmd='vtk_test',
+                          vcp_name='vtk_test',
+                          vcp_version=__version__)
+
+    qtpyvcp.run_vcp(opts, VCP_CONFIG_FILE)
+
+
+if __name__ == '__main__':
+    main()
---
+++ video_tests/vtk_test/mainwindow.py
@@ -0,0 +1,15 @@
+from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
+
+# Setup logging
+from qtpyvcp.utilities import logger
+LOG = logger.getLogger('qtpyvcp.' + __name__)
+
+class MyMainWindow(VCPMainWindow):
+    """Main window class for the VCP."""
+    def __init__(self, *args, **kwargs):
+        super(MyMainWindow, self).__init__(*args, **kwargs)
+
+    # add any custom methods here
+    def on_exitBtn_clicked(self):
+        self.app.quit()
+
