3cd47274537a4bbf5195939cc212a22ec4d5cc4d
Kurt Jacobson <kcjengr@gmail.com>
2019-03-27 14:14:52 -0400

    fix module imports in yaml config

--- examples/atc_test/atc_test/mainwindow.py
+++ examples/atc_test/atc_test/mainwindow.py
@@ -4,9 +4,9 @@ from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 from qtpyvcp.utilities import logger
 LOG = logger.getLogger('qtpyvcp.' + __name__)
 
-class MyMainWindow(VCPMainWindow):
+class MainWindow(VCPMainWindow):
     """Main window class for the VCP."""
     def __init__(self, *args, **kwargs):
-        super(MyMainWindow, self).__init__(*args, **kwargs)
+        super(MainWindow, self).__init__(*args, **kwargs)
 
     # add any custom methods here
