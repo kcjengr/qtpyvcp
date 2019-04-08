3078f8794af60acfc5a0956d0c60e92b507fe7ee
Kurt Jacobson <kcjengr@gmail.com>
2019-04-08 19:44:59 -0400

    don't use IO pins on HalCheckBox

--- qtpyvcp/widgets/hal_widgets/hal_checkbox.py
+++ qtpyvcp/widgets/hal_widgets/hal_checkbox.py
@@ -4,9 +4,16 @@ HAL CheckBox
 
 CheckBox used for displaying and setting BIT HAL pin values.
 
-Generates two I/O pins, ``qtpyvcp.checkbox-name.enabled`` which indicates
-if the checkbox is enabled, and ``qtpyvcp.checkbox-name.checked`` which
-indicates if the checkbox is checked.
+Generated HAL Pins
+++++++++++++++++++
+
+========================= ===== =========
+HAL Pin Name              Type  Direction
+========================= ===== =========
+qtpyvcp.checkbox.enable   bit   in
+qtpyvcp.checkbox.check    bit   in
+qtpyvcp.checkbox.checked  bit   out
+========================= ===== =========
 """
 
 from qtpy.QtWidgets import QCheckBox
@@ -21,16 +28,17 @@ class HalCheckBox(QCheckBox, HALWidget):
     def __init__(self, parent=None, pin_name=None):
         super(HalCheckBox, self).__init__(parent)
 
-        self._enabled_pin = None
+        self._enable_pin = None
+        self._check_pin = None
         self._checked_pin = None
 
         self.toggled.connect(self.onCheckedStateChanged)
 
     def changeEvent(self, event):
         super(HalCheckBox, self).changeEvent(event)
-        if event == QEvent.EnabledChange and self._enabled_pin is not None:
+        if event == QEvent.EnabledChange and self._enable_pin is not None:
             print "enabled changed:", self.isEnabled()
-            self._enabled_pin.value = self.isEnabled()
+            self._enable_pin.value = self.isEnabled()
 
     def onCheckedStateChanged(self, checked):
         print "checked changed:", checked
@@ -41,12 +49,16 @@ class HalCheckBox(QCheckBox, HALWidget):
         comp = hal.COMPONENTS['qtpyvcp']
         obj_name = str(self.objectName()).replace('_', '-')
 
-        # add checkbox.enabled HAL pin
-        self._enabled_pin = comp.addPin(obj_name + ".enabled", "bit", "io")
-        self._enabled_pin.value = self.isEnabled()
-        self._enabled_pin.valueChanged.connect(lambda enable: self.setEnabled(enable))
+        # add checkbox.enable HAL pin
+        self._enable_pin = comp.addPin(obj_name + ".enable", "bit", "in")
+        self._enable_pin.value = self.isEnabled()
+        self._enable_pin.valueChanged.connect(self.setEnabled)
+
+        # add checkbox.check HAL pin
+        self._check_pin = comp.addPin(obj_name + ".check", "bit", "in")
+        self._check_pin.value = self.isChecked()
+        self._check_pin.valueChanged.connect(self.setChecked)
 
         # add checkbox.checked HAL pin
-        self._checked_pin = comp.addPin(obj_name + ".checked", "bit", "io")
+        self._checked_pin = comp.addPin(obj_name + ".checked", "bit", "out")
         self._checked_pin.value = self.isChecked()
-        self._checked_pin.valueChanged.connect(lambda check: self.setChecked(check))
