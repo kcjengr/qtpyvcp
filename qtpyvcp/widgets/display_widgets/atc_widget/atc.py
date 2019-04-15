696ca4a59c37c866c9bac4cb10d785c1c6fd909d
TurBoss <j.l.toledano.l@gmail.com>
2019-04-15 06:55:04 +0200

    new remap M14

--- qtpyvcp/widgets/display_widgets/atc_widget/atc.py
+++ qtpyvcp/widgets/display_widgets/atc_widget/atc.py
@@ -10,7 +10,6 @@ ctypes.CDLL(ctypes.util.find_library("GL"), mode=ctypes.RTLD_GLOBAL)
 # end of Workarround
 
 
-import linuxcnc
 from qtpy.QtCore import Signal, Slot, QUrl, QTimer
 from qtpy.QtQuickWidgets import QQuickWidget
 
@@ -45,14 +44,18 @@ class DynATC(QQuickWidget):
         if IN_DESIGNER:
             return
 
-        self.command = linuxcnc.command()
-
         self.atc_position = 0
         self.pocket = 1
         self.home = 0
         self.homing = 0
-
+        self.pocket_slots = 12
         self.component = QComponent("atc-widget")
+
+        # define pocket pins to store tools
+
+        for i in range(self.pocket_slots):
+            pin_name = "pocket-{}".format(i+1)
+            self.component.newPin(pin_name, "s32", "in")
 
         self.component.newPin('home', "float", "in")
         self.component.newPin('homing', "float", "in")
@@ -79,38 +82,16 @@ class DynATC(QQuickWidget):
 
         self.component.ready()
 
-        inifile = os.getenv("INI_FILE_NAME")
-        self.inifile = linuxcnc.ini(inifile)
-
-        self.parameter_file = self.inifile.find("RS274NGC", "PARAMETER_FILE")
-
         self.engine().rootContext().setContextProperty("atc_spiner", self)
         qml_path = os.path.join(WIDGET_PATH, "atc.qml")
         url = QUrl.fromLocalFile(qml_path)
 
         self.setSource(url)  # Fixme fails on qtdesigner
 
-        self.parameter = dict()
-
         self.tool_table = None
         self.status_tool_table = None
         self.pockets = dict()
         self.tools = None
-
-        self.offsets = [
-            5190,
-            5191,
-            5192,
-            5193,
-            5194,
-            5195,
-            5196,
-            5197,
-            5198,
-            5199,
-            5200,
-            5201
-        ]
 
         self.load_tools()
         self.draw_tools()
@@ -124,20 +105,17 @@ class DynATC(QQuickWidget):
 
     def load_tools(self):
 
-        with open(self.parameter_file) as param:
-            for line in param.read().splitlines():
-                offset = int(line[0:4])
-                val = float(line[5:])
-                self.parameter[offset] = val
-
         self.tool_table = TOOLTABLE.getToolTable()
         self.status_tool_table = STATUS.tool_table
 
         self.pockets = dict()
         self.tools = dict()
 
-        for index, offset in enumerate(self.offsets):
-            self.pockets[index + 1] = self.parameter[offset]
+        for i in range(self.pocket_slots):
+            pin_name = "pocket-{}".format(i+1)
+            print(pin_name)
+
+            self.pockets[i + 1] = self.component[pin_name].value
 
     def draw_tools(self):
         for i in range(1, 13):
