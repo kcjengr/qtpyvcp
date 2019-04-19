a731dd32b35fe3ddd00ef88c73e00a617dfb2c3d
Kurt Jacobson <kcjengr@gmail.com>
2019-04-18 23:50:11 -0400

    ENH: HALButton - add pulse on press option

--- qtpyvcp/widgets/hal_widgets/hal_button.py
+++ qtpyvcp/widgets/hal_widgets/hal_button.py
@@ -15,6 +15,7 @@ qtpyvcp.button.out        bit   out
 ========================= ===== =========
 """
 
+from qtpy.QtCore import Property, QTimer
 from qtpy.QtWidgets import QPushButton
 
 from qtpyvcp import hal
@@ -31,16 +32,40 @@ class HalButton(QPushButton, HALWidget):
         self._enable_pin = None
         self._pressed_pin = None
 
+        self._pulse = False
+        self._pulse_duration = 100
+        self.pulse_timer = None
+
         self.pressed.connect(self.onPress)
         self.released.connect(self.onRelease)
 
     def onPress(self):
         if self._pressed_pin is not None:
             self._pressed_pin.value = True
+        if self._pulse:
+            self.pulse_timer.start(self._pulse_duration)
 
     def onRelease(self):
         if self._pressed_pin is not None:
             self._pressed_pin.value = False
+
+    @Property(bool)
+    def pulseOnPress(self):
+        """Whether to pulse the pin value on press."""
+        return self._pulse
+
+    @pulseOnPress.setter
+    def pulseOnPress(self, pulse):
+        self._pulse = pulse
+
+    @Property(int)
+    def pulseDuration(self):
+        """Pulse duration on ms."""
+        return self._pulse_duration
+
+    @pulseDuration.setter
+    def pulseDuration(self, duration):
+        self._pulse_duration = duration
 
     def initialize(self):
         comp = hal.COMPONENTS['qtpyvcp']
@@ -53,3 +78,8 @@ class HalButton(QPushButton, HALWidget):
 
         # add button.out HAL pin
         self._pressed_pin = comp.addPin(obj_name + ".out", "bit", "out")
+
+        if self._pulse:
+            self.pulse_timer = QTimer()
+            self.pulse_timer.setSingleShot(True)
+            self.pulse_timer.timeout.connect(self.onRelease)
