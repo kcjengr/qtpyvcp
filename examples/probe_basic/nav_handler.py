a3933419d5297102815892355a5bfaf4f2f53cb2
Kurt Jacobson <kcjengr@gmail.com>
2019-04-21 00:08:54 -0400

    use nav button group to select probe stake page

--- examples/probe_basic/nav_handler.py
+++
@@ -1,7 +0,0 @@
-from functools import partial
-
-def setupNav(parent):
-    parent.probetabGroup.buttonClicked.connect(partial(probetabChangePage, parent))
-    if button.property('buttonName'):
-        getattr(parent, button.property('buttonName')).setChecked(True)
-
