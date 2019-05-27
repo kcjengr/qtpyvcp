9ba84e7ea7e31b13d6ab799080122571555377b5
TurBoss <j.l.toledano.l@gmail.com>
2019-05-27 21:40:46 +0200

    fix initi property

--- qtpyvcp/widgets/input_widgets/gcode_editor.py
+++ qtpyvcp/widgets/input_widgets/gcode_editor.py
@@ -263,7 +263,7 @@ class GcodeEditor(EditorBase, QObject):
         self.last_line = None
         # self.setEolVisibility(True)
 
-        self._editor = False
+        self.editor = False
 
         # QSS Hack
 
