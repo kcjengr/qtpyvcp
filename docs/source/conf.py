cd35d22ab2dc27980b0c04340a179a4edf5992ce
Kurt Jacobson <kcjengr@gmail.com>
2020-01-25 12:54:48 -0500

    DOC: update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'QtPyVCP'
-copyright = u'2018-2019, Kurt Jacobson.'
+copyright = u'2018-2020, Kurt Jacobson.'
 author = u'Kurt Jacobson'
 
 # The version info for the project you're documenting, acts as replacement for
