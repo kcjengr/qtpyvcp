b4016807f6df59c89d89f3d72f8c6de5f7a81472
Kurt Jacobson <kcjengr@gmail.com>
2020-02-16 14:44:26 -0500

    fix error when requesting non defined keyboard

--- qtpyvcp/widgets/virtual_keyboards/__init__.py
+++ qtpyvcp/widgets/virtual_keyboards/__init__.py
@@ -1,5 +1,10 @@
 
 from qtpyvcp import KEYBOARDS
 
-def getKeyboard(vkb_name):
-    return KEYBOARDS.get(vkb_name)
+
+def getKeyboard(vkb_name, fallback='default'):
+    try:
+        return KEYBOARDS[vkb_name]
+    except KeyError:
+        default = KEYBOARDS['default']
+        return KEYBOARDS.get(fallback, default)
