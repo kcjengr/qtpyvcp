6bb968aa6cbc716ec33aceccbfae42930ba3df98
Kurt Jacobson <kcjengr@gmail.com>
2020-04-20 08:51:04 -0400

    fix import error with designer

--- qtpyvcp/__init__.py
+++ qtpyvcp/__init__.py
@@ -12,6 +12,8 @@ del get_versions
 QTPYVCP_DIR = os.path.abspath(os.path.dirname(__file__))
 TOP_DIR = os.path.dirname(QTPYVCP_DIR)
 
+DEFAULT_CONFIG_FILE = os.path.join(QTPYVCP_DIR, 'yaml_lib/default_config.yml')
+
 # globals
 CONFIG = {}
 OPTIONS = DotDict()
