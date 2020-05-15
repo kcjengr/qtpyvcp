d8361b154c25848d1f33c6ac543ccd5549926dfe
James <james.walker.nz@me.com>
2020-05-15 15:15:49 +1200

    Material changes: - improving doc strings - adding base functionality - raw queue model working

--- qtpyvcp/widgets/input_widgets/mdihistory_widget.py
+++ qtpyvcp/widgets/input_widgets/mdihistory_widget.py
@@ -1,20 +1,38 @@
-"""MDI History Widget
-Maintains MDI history and implements a queue system to support
-entering in new MDI commands while others are being executed.
+# -*- coding: utf-8 -*-
+"""QtPyVCP MDI History Widget
+
+This widget implements the following key elements:
+[1] A history display of MDI commands issued with the latest
+command at the top of the list and the oldest at the bottom
+of the list.
+
+[2] A queue system of commands that have been entered
+but have not yet been executed. This allows the rapid entry
+of MDI commands to be executed without having to wait for any
+running commands to complete.
+
+ToDo:
+    * add/test for styling based on the class queue codes
+    * be able to select and remove unwanted commands from the history
+    * be able to select a row and run commands from that point upwards
+
 """
-from qtpy.QtCore import Qt, Slot, Property
-#from qtpy.QtGui import QValidator
+
+from qtpy.QtCore import Qt, Slot, Property, QTimer
 from qtpy.QtWidgets import QListWidget
 from qtpy.QtWidgets import QListWidgetItem
 
+import qtpyvcp
 from qtpyvcp.plugins import getPlugin
 from qtpyvcp.utilities.info import Info
 from qtpyvcp.actions.machine_actions import issue_mdi
 from qtpyvcp.widgets.base_widgets.base_widget import CMDWidget
-from qtpyvcp.widgets.input_widgets.mdientry_widget import MDIEntry
+
+import linuxcnc
 
 
 STATUS = getPlugin('status')
+STAT = STATUS.stat
 INFO = Info()
 
 
@@ -28,6 +46,8 @@ class MDIHistory(QListWidget, CMDWidget):
     Visual style is used to identify items that have been completed,
     are running and are yet to run.
     """
+
+    # MDI Queue status constants
     MDIQ_DONE = 0
     MDIQ_RUNNING = 1
     MDIQ_TODO = 2
@@ -36,10 +56,9 @@ class MDIHistory(QListWidget, CMDWidget):
     def __init__(self, parent=None):
         super(MDIHistory, self).__init__(parent)
 
-        print 'init parent = {}'.format(parent)
-
         self.mdi_entryline_name = None
         self.mdi_entry_widget = None
+        self.heart_beat_timer = None
 
         #self.returnPressed.connect(self.submit)
 
@@ -67,19 +86,8 @@ class MDIHistory(QListWidget, CMDWidget):
         row_item.setData(MDIHistory.MDQQ_ROLE, MDIHistory.MDIQ_TODO)
         self.insertItem(0, row_item)
 
-        # scan for the next command to execute from bottom up.
-        list_length = self.count()-1
-        while list_length >= 0:
-            row_item = self.item(list_length)
-            row_item_data = row_item.data(MDIHistory.MDQQ_ROLE)
-            if row_item_data == MDIHistory.MDIQ_TODO:
-                print "Item found to execute on: {}".format(row_item.text())
-                break
-            list_length -= 1
-
-
-        #issue_mdi(cmd)
-        #STATUS.mdi_history.setValue(cmd)
+        # now clear down the mdi entry text ready for new input
+        self.mdi_entry_widget.clear()
 
     def row_clicked(self):
         """Item row clicked."""
@@ -119,18 +127,51 @@ class MDIHistory(QListWidget, CMDWidget):
             self.addItem(row_item)
 
 
-        #self.addItems(history_list)
+    def heart_beat(self):
+        """Supports heart beat on the MDI History execution queue."""
+        # check if machine is idle and ready to run another command
+        if STAT.interp_state != linuxcnc.INTERP_IDLE:
+            # RS274NGC interpreter not in a state to execute
+            print
+            return
+
+        # scan for the next command to execute from bottom up.
+        list_length = self.count()-1
+        while list_length >= 0:
+            row_item = self.item(list_length)
+            row_item_data = row_item.data(MDIHistory.MDQQ_ROLE)
+
+            if row_item_data == MDIHistory.MDIQ_RUNNING:
+                # machine is in idle state so the running command is done
+                row_item.setData(MDIHistory.MDQQ_ROLE, MDIHistory.MDIQ_DONE)
+
+            elif row_item_data == MDIHistory.MDIQ_TODO:
+                cmd = str(row_item.text()).strip()
+                row_item.setData(MDIHistory.MDQQ_ROLE, MDIHistory.MDIQ_RUNNING)
+                print "Item found to execute on: {}".format(cmd)
+                issue_mdi(cmd)
+                break
+
+            list_length -= 1
 
     def initialize(self):
         """Load up starting data and set signal connections."""
         history = STATUS.mdi_history.value
         self.set_history(history)
         self.clicked.connect(self.row_clicked)
-        #STATUS.mdi_history.notify(self.set_history)
-        self.mdi_entry_widget = self.parent().findChild(MDIEntry, self.mdi_entryline_name)
-        #print 'find child: {}'.format(self.parent().findChild(MDIEntry, 'mdiEntry'))
+
+        # get handle to windows list and seach through them
+        # for the widget referenced in mdi_entryline_name
+        for win_name, obj in qtpyvcp.WINDOWS.items():
+            if hasattr(obj, self.mdi_entryline_name):
+                self.mdi_entry_widget = getattr(obj, self.mdi_entryline_name)
+
+        # setup the basic timer system as a heart beat on the queue
+        self.heart_beat_timer = QTimer(self)
+        self.heart_beat_timer.start(1000)
+        self.heart_beat_timer.timeout.connect(self.heart_beat)
 
 
     def terminate(self):
         """Teardown processing."""
-        pass
+        self.heart_beat_timer.stop()
