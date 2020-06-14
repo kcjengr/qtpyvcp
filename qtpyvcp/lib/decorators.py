25694fa193c3685dfa64792845f9dd7cfe526465
Kurt Jacobson <kcjengr@gmail.com>
2020-06-13 22:42:12 -0400

    ENH: Decorators - Add support for marking classes as deprecated

--- qtpyvcp/lib/decorators.py
+++ qtpyvcp/lib/decorators.py
@@ -1,24 +1,50 @@
+
+import inspect
+
+from qtpy.QtWidgets import QWidget
 from qtpyvcp.utilities.logger import getLogger
 
 LOG = getLogger(__name__)
 
 
-def deprecated(reason='', replaced_by=''):
-    msg_temp = 'Deprecation Warning: {mod}.{func}() has been deprecated ' \
-               '{reason}and will be removed in the future.'
-    if replaced_by:
-        msg_temp += ' Use {}() instead'.format(replaced_by)
+def deprecated(reason='Not Specified', replaced_by='Not Specified'):
 
-    def decorator(func):
+    def decorator(obj):
 
-        msg = msg_temp.format(mod=func.__module__,
-                              func=func.__name__,
-                              reason=reason if reason == '' else reason + ', ')
+        if inspect.isclass(obj) and issubclass(obj, QWidget):
+            msg_temp = 'Deprecation Warning: The widget "{mod}.{cls}" has ' \
+                       'been deprecated and will be removed in the future. ' \
+                       'Reason: {reason}. ' \
+                       'Replaced by: {replaced_by}.'
 
-        def inner(*args, **kwargs):
-            LOG.warn(msg)
-            return func(*args, **kwargs)
+            msg = msg_temp.format(mod=obj.__module__,
+                                  cls=obj.__name__,
+                                  reason=reason,
+                                  replaced_by=replaced_by)
 
-        return inner
+            LOG.warning(msg)
+
+            return obj
+
+        elif inspect.isfunction(obj):
+
+            msg_temp = 'Deprecation Warning: The function {mod}.{func}() has ' \
+                       'been deprecated and will be removed in the future. ' \
+                       'Reason: {reason}. ' \
+                       'Replaced by: {replaced_by}.'
+
+            msg = msg_temp.format(mod=obj.__module__,
+                                  func=obj.__name__,
+                                  reason=reason,
+                                  replaced_by=replaced_by)
+
+            def inner(*args, **kwargs):
+                LOG.warn(msg)
+                return obj(*args, **kwargs)
+
+            return inner
+
+        else:
+            return obj
 
     return decorator
--- qtpyvcp/widgets/display_widgets/load_meter.py
+++ qtpyvcp/widgets/display_widgets/load_meter.py
@@ -1,2 +1,9 @@
-#for backward compatibility with OLD VCPs
-from bar_indicator import BarIndicator as LoadMeter
+# for backward compatibility with older VCPs
+from qtpyvcp.lib.decorators import deprecated
+from bar_indicator import BarIndicator
+
+
+@deprecated(reason="new base class implementation", replaced_by="BarIndicator")
+class LoadMeter(BarIndicator):
+    def __init__(self, parent=None):
+        super(LoadMeter, self).__init__(parent=parent)
