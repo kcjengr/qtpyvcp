3444ef060a37100774c4f882fba40302db75a19e
Kurt Jacobson <kcjengr@gmail.com>
2020-06-14 14:10:51 -0400

    use global DRO format settings

--- qtpyvcp/widgets/base_widgets/dro_base_widget.py
+++ qtpyvcp/widgets/base_widgets/dro_base_widget.py
@@ -7,6 +7,8 @@ DROBaseWidget
 from qtpy.QtCore import Slot, Property
 
 from qtpyvcp.plugins import getPlugin
+from qtpyvcp.utilities.settings import getSetting
+
 from qtpyvcp.widgets import VCPWidget
 from qtpyvcp.utilities import logger
 
@@ -37,7 +39,6 @@ class DROBaseWidget(VCPWidget):
     """DROBaseWidget
 
     This class implements the basic functionality needed for DRO widgets.
-
     """
 
     def __init__(self):
@@ -50,8 +51,20 @@ class DROBaseWidget(VCPWidget):
         self._ref_typ = RefType.Relative
         self._mm_fmt = '%10.3f'
         self._in_fmt = '%9.4f'
+        self._deg_fmt = '%10.3f' #'%(deg) %(min)\' %(sec)"'
+
+        self._use_global_fmt_settings = True
+
+        # settings
+        self.in_fmt_setting = None
+        self.mm_fmt_setting = None
+        self.deg_fmt_setting = None
+
+        self._angular_axis = False
+
 
         self._fmt = self._in_fmt
+        self._conv = 1
 
         self.updateValue()
 
@@ -65,13 +78,18 @@ class DROBaseWidget(VCPWidget):
                 If no unit type is specified the current program units will
                 be used.
         """
-        if units is None:
-            units = str(self.status.program_units)
 
-        if units == 'in':
-            self._fmt = self._in_fmt
+        if self._angular_axis:
+            self._fmt = self._deg_fmt
+
         else:
-            self._fmt = self._mm_fmt
+            if units is None:
+                units = str(self.status.program_units)
+
+            if units == 'in':
+                self._fmt = self._in_fmt
+            else:
+                self._fmt = self._mm_fmt
 
         # force update
         self.updateValue()
@@ -80,10 +98,24 @@ class DROBaseWidget(VCPWidget):
         getattr(self.pos, RefType.toString(self._ref_typ)).notify(self.updateValue)
         self.updateValue()
 
+        if self._use_global_fmt_settings:
+
+            self.in_fmt_setting = getSetting('dro.inch-format')
+            self.mm_fmt_setting = getSetting('dro.millimeter-format')
+            self.deg_fmt_setting = getSetting('dro.degree-format')
+
+            try:
+                self.in_fmt_setting.notify(lambda fmt: self.setProperty('inchFormat', fmt))
+                self.mm_fmt_setting.notify(lambda fmt: self.setProperty('millimeterFormat', fmt))
+                self.deg_fmt_setting.notify(lambda fmt: self.setProperty('degreeFormat', fmt))
+            except AttributeError:  # settings not found
+                pass
+
     def updateValue(self, pos=None):
-        """Refresh displayed text, used in designer."""
+        """Update the displayed position."""
         if pos is None:
             pos = getattr(self.pos, RefType.toString(self._ref_typ)).getValue()
+
         self.setText(self._fmt % pos[self._anum])
 
     @Property(int)
@@ -102,6 +134,9 @@ class DROBaseWidget(VCPWidget):
     @axisNumber.setter
     def axisNumber(self, axis):
         self._anum = axis
+        if axis in [3, 4, 5]:
+            self._angular_axis = True
+            self._fmt = self._deg_fmt
         self.updateValue()
 
     @Property(str)
@@ -109,15 +144,65 @@ class DROBaseWidget(VCPWidget):
         return self._in_fmt
 
     @inchFormat.setter
-    def inchFormat(self, inch_format):
-        self._in_fmt = inch_format
+    def inchFormat(self, in_fmt):
+        if in_fmt == '' and self.mm_fmt_setting:
+            self.in_fmt_setting.resetValue()
+
+        else:
+            try:
+                txt = in_fmt % 360.42
+                self._in_fmt = in_fmt
+            except:
+                if self.in_fmt_setting:
+                    self.in_fmt_setting.setValue(self._in_fmt)
+
         self.updateUnits()
 
     @Property(str)
-    def metricFormat(self):
+    def millimeterFormat(self):
         return self._mm_fmt
 
-    @metricFormat.setter
-    def metricFormat(self, metric_format):
-        self._mm_fmt = metric_format
+    @millimeterFormat.setter
+    def millimeterFormat(self, mm_fmt):
+        if mm_fmt == '' and self.mm_fmt_setting:
+            self.mm_fmt_setting.resetValue()
+
+        else:
+            try:
+                txt = mm_fmt % 360.42
+                self._mm_fmt = mm_fmt
+            except:
+                if self.mm_fmt_setting:
+                    self.mm_fmt_setting.setValue(self._mm_fmt)
+
         self.updateUnits()
+
+    # ToDO: Add support for DMS angle notation
+    # https://stackoverflow.com/questions/2579535
+    @Property(str)
+    def degreeFormat(self):
+        return self._deg_fmt
+
+    @degreeFormat.setter
+    def degreeFormat(self, deg_fmt):
+
+        if deg_fmt == '' and self.deg_fmt_setting:
+            self.deg_fmt_setting.resetValue()
+
+        else:
+            try:
+                txt = deg_fmt % 360.42
+                self._deg_fmt = deg_fmt
+            except:
+                if self.deg_fmt_setting:
+                    self.deg_fmt_setting.setValue(self._deg_fmt)
+
+        self.updateUnits()
+
+    @Property(bool)
+    def useGlobalDroFormatSettings(self):
+        return self._use_global_fmt_settings
+
+    @useGlobalDroFormatSettings.setter
+    def useGlobalDroFormatSettings(self, use_fmt_settings):
+        self._use_global_fmt_settings = use_fmt_settings
