2c9cb4c4e211c356d7be99f127f3b55be5d0f0e0
Kurt Jacobson <kcjengr@gmail.com>
2020-07-11 23:28:31 -0400

    define gcode syntax in yml file

--- qtpyvcp/widgets/input_widgets/gcedit.py
+++ qtpyvcp/widgets/input_widgets/gcedit.py
@@ -1,5 +1,5 @@
 
-import sys
+import sys, os
 from PyQt5.QtWidgets import (QApplication, QPlainTextEdit,
     QTextEdit, QWidget, QMenu)
 from PyQt5.QtGui import (QIcon, QFontDatabase, QFont, QColor, QPainter,
@@ -12,159 +12,47 @@ from qtpyvcp import actions
 from qtpyvcp.plugins import getPlugin
 STATUS = getPlugin('status')
 
+import yaml
+
+from qtpyvcp import DEFAULT_CONFIG_FILE
+YAML_DIR = os.path.dirname(DEFAULT_CONFIG_FILE)
+
 
 class gCodeHighlight(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super(gCodeHighlight, self).__init__(parent)
 
-        # G codes by modal group
-        # Modal Group 0 Non-Modal
-        group0Format = QTextCharFormat()
-        group0Format.setForeground(QColor('#800000')) # Maroon
-        group0Format.setFontWeight(QFont.Bold)
-        group0Patterns = ['G4', 'G10', 'G28',\
-            'G30', 'G52', 'G53', 'G92',\
-            'G92.1', 'G92.2', 'G92.3']
+        self.rules = []
 
-        # Modal Group 1 Motion
-        group1Format = QTextCharFormat()
-        group1Format.setForeground(QColor('#FF0000')) # Red
-        group1Format.setFontWeight(QFont.Bold)
-        group1Patterns = ['G0', 'G1(?!\d)', 'G2(?!\d)', 'G3(?!\d)',\
-            'G33', 'G38.2', 'G38.3', 'G38.4',\
-            'G38.5', 'G73', 'G76', 'G80',\
-            'G81', 'G82', 'G83', 'G84', 'G85',\
-            'G86', 'G87', 'G88', 'G89']
+        self.loadSyntaxFromYAML()
 
-        # Modal Group 2 Plane selection
-        group2Format = QTextCharFormat()
-        group2Format.setForeground(QColor('#FFA500')) # Orange
-        group2Format.setFontWeight(QFont.Bold)
-        group2Patterns = ['G17', 'G18', 'G19',\
-            'G17.1', 'G18.1', 'G19.1']
+    def loadSyntaxFromYAML(self):
 
-        # Modal Group 3 Distance Mode
-        group3Format = QTextCharFormat()
-        group3Format.setForeground(QColor('#808000')) # Olive
-        group3Format.setFontWeight(QFont.Bold)
-        group3Patterns = ['G90', 'G91']
+        with open(os.path.join(YAML_DIR, 'gcode_syntax.yml')) as fh:
+            syntax_spec = yaml.load(fh, Loader=yaml.FullLoader)
 
-        # Modal Group 4 Arc IJK Distance Mode
-        group4Format = QTextCharFormat()
-        group4Format.setForeground(QColor('#008000')) # Green
-        group4Format.setFontWeight(QFont.Bold)
-        group4Patterns = ['G90.1', 'G91.1']
+        syntax_spec = syntax_spec['syntaxSpec']
+        cio = QRegularExpression.CaseInsensitiveOption
 
-        # Modal Group 5 Feed Rate Mode
-        group5Format = QTextCharFormat()
-        group5Format.setForeground(QColor('#800080')) # Purple
-        group5Format.setFontWeight(QFont.Bold)
-        group5Patterns = ['G93', 'G94', 'G95']
+        for name, spec in syntax_spec['definitions'].items():
+            char_fmt = QTextCharFormat()
 
-        # Modal Group 6 Units
-        group6Format = QTextCharFormat()
-        group6Format.setForeground(QColor('#FF00FF')) # Fuchsia
-        group6Format.setFontWeight(QFont.Bold)
-        group6Patterns = ['G20', 'G21']
+            for option, value in spec.get('textFormat', {}).items():
+                if value is None:
+                    continue
 
-        # Modal Group 7 Cutter Diameter Compensation
-        group7Format = QTextCharFormat()
-        group7Format.setForeground(QColor('#A52A2A')) # Brown
-        group7Format.setFontWeight(QFont.Bold)
-        group7Patterns = ['G40', 'G41', 'G42',\
-            'G41.1', 'G42.1']
+                if option in ['foreground', 'background']:
+                    value = QColor(value)
 
-        # Modal Group 8 Tool Length Offset
-        group8Format = QTextCharFormat()
-        group8Format.setForeground(QColor('#008080')) # Teal
-        group8Format.setFontWeight(QFont.Bold)
-        group8Patterns = ['G43', 'G43.1', 'G49']
+                if isinstance(value, basestring) and value.startswith('QFont:'):
+                    value = getattr(QFont, value.lstrip('QFont:'))
 
-        # Modal Group 10 Canned Cycles Return Mode
-        group10Format = QTextCharFormat()
-        group10Format.setForeground(QColor('#00FFFF')) # Aqua
-        group10Format.setFontWeight(QFont.Bold)
-        group10Patterns = ['G98', 'G99']
+                attr = 'set' + option[0].capitalize() + option[1:]
+                getattr(char_fmt, attr)(value)
 
-        # Modal Group 12 Coordinate System
-        group12Format = QTextCharFormat()
-        group12Format.setForeground(QColor('#0000FF')) # Blue
-        group12Format.setFontWeight(QFont.Bold)
-        group12Patterns = ['G54', 'G55', 'G56',\
-            'G57', 'G58', 'G59', 'G59.1',\
-            'G59.2', 'G59.3']
-
-        # Modal Group 13 Control Mode
-        group13Format = QTextCharFormat()
-        group13Format.setForeground(QColor('#000080')) # Navy
-        group13Format.setFontWeight(QFont.Bold)
-        group13Patterns = ['G61', 'G61.1', 'G64']
-
-        # Modal Group 14 Spindle Speed Mode
-        group14Format = QTextCharFormat()
-        group14Format.setForeground(QColor('#808080')) # Gray
-        group14Format.setFontWeight(QFont.Bold)
-        group14Patterns = ['G96', 'G97']
-
-        # Modal Group 15 Lathe Diameter Mode
-        group15Format = QTextCharFormat()
-        group15Format.setForeground(QColor('#C0C0C0')) # Silver
-        group15Format.setFontWeight(QFont.Bold)
-        group15Patterns = ['G7(?!\d)', 'G8(?!\d)']
-
-        # M Codes
-        mCodesFormat = QTextCharFormat()
-        mCodesFormat.setForeground(QColor('#FF4500')) # Orange Red
-        mCodesFormat.setFontWeight(QFont.Bold)
-        mCodesPatterns = ['M\d{1,3}']
-
-        # Other Codes
-        otherCodesFormat = QTextCharFormat()
-        otherCodesFormat.setForeground(QColor('#006400')) # Dark Green
-        otherCodesFormat.setFontWeight(QFont.Bold)
-        otherCodesPatterns = ['F\\d+', 'S\\d+', 'T\\d+']
-
-        # Comments
-        commentFormat = QTextCharFormat()
-        commentFormat.setForeground(QColor('#A9A9A9')) # Dark Gray
-        commentFormat.setFontWeight(QFont.Bold)
-        commentPatterns = [';.*', '\\(.*\\)']
-
-
-        self.cio = QRegularExpression.CaseInsensitiveOption
-        self.rules = [(QRegularExpression(item, self.cio), group0Format) for item in group0Patterns]
-        for item in group1Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group1Format])
-        for item in group2Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group2Format])
-        for item in group3Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group3Format])
-        for item in group4Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group4Format])
-        for item in group5Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group5Format])
-        for item in group6Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group6Format])
-        for item in group7Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group7Format])
-        for item in group8Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group8Format])
-        for item in group10Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group10Format])
-        for item in group12Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group12Format])
-        for item in group13Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group13Format])
-        for item in group14Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group14Format])
-        for item in group15Patterns:
-            self.rules.append([QRegularExpression(item, self.cio), group15Format])
-        for item in mCodesPatterns:
-            self.rules.append([QRegularExpression(item, self.cio), mCodesFormat])
-        for item in otherCodesPatterns:
-            self.rules.append([QRegularExpression(item, self.cio), otherCodesFormat])
-        for item in commentPatterns:
-            self.rules.append([QRegularExpression(item, self.cio), commentFormat])
+            patterns = spec.get('match', '')
+            for pattern in patterns:
+                self.rules.append([QRegularExpression(pattern, cio), char_fmt])
 
     def highlightBlock(self, text):  # Using QRegularExpression
         for exp, format in self.rules:
@@ -231,7 +119,6 @@ class gCodeEdit(QPlainTextEdit):
 
         # file operations
         gCodeFileName = ''
-        self.show()
 
     @Slot(bool)
     def EditorReadOnly(self, state):
