d2628855f2d7684916756cc1944eb6625ca4c926
TurBoss <j.l.toledano.l@gmail.com>
2021-02-19 22:28:51 +0100

    fix release script

--- .scripts/publish_pypi_release.sh
+++ .scripts/publish_pypi_release.sh
@@ -6,7 +6,7 @@ echo 'Installing twine... '
 pip install twine
 
 echo -n 'Using setuptools version: '
-python -c "import setuptools; print setuptools.__version__"
+python -c "import setuptools; print(setuptools.__version__)"
 
 echo 'Uploading files to PyPi...'
 twine upload \
