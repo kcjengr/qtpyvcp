e88fb3ac28053aa40b939e9571ac926a8483bfb8
TurBoss <j.l.toledano.l@gmail.com>
2021-03-23 03:50:17 +0100

    tool table examples

---
+++ qtpyvcp/plugins/database/tool_table.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey, Float, Text
+from sqlalchemy.orm import relationship
+
+from base import Base
+
+
+class ToolTable(Base):
+    __tablename__ = 'tool_table'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    tools = relationship("Tool")
+
+        
+class Tool(Base):
+    __tablename__ = 'tool'
+    
+    id = Column(Integer, primary_key=True)
+    remark = Column(Text)
+    tool_no = Column(Integer)
+    pocket = Column(Integer)
+    x_offset = Column(Float)
+    y_offset = Column(Float)
+    z_offset = Column(Float)
+    a_offset = Column(Float)
+    b_offset = Column(Float)
+    c_offset = Column(Float)
+    u_offset = Column(Float)
+    v_offset = Column(Float)
+    w_offset = Column(Float)
+    diameter = Column(Float)
+    tool_table_id = Column(Integer, ForeignKey('tool_table.id'))
+    tool_table = relationship("ToolTable", back_populates="tools")
+
