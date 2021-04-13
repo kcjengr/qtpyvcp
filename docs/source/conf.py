b81a988444d945ff6bfc6e3490a42c0632e2e4a7
TurBoss <j.l.toledano.l@gmail.com>
2021-04-13 17:29:51 +0200

    don't set path in python

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,6 @@ import os
 import sys
 import mock
 
-# qtpyvcp_dir = os.path.join(os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../..'))
-
 import qtpyvcp
 
 # -- General configuration ------------------------------------------------
