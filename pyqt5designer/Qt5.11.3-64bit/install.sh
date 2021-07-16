285d12dcf66609932e024c28806c4addac3c548e
TurBoss <j.l.toledano.l@gmail.com>
2021-07-16 22:29:14 +0200

    compile libpyqt5 for python 3.9 debian 11 (bullseye)

---
+++ pyqt5designer/Qt5.11.3-64bit/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+LIB_PATH="/usr/lib/x86_64-linux-gnu/qt5/plugins/designer"
+
+OLD_LIB=$LIB_PATH/"libpyqt5.so"
+if [ -f "$OLD_LIB" ]
+then
+    echo "renaming existing libpyqt5.so to libpyqt5.so.old"
+    mv "$OLD_LIB" "$OLD_LIB.old"
+fi
+
+# echo "copying libpyqt5_py2.so to $LIB_PATH"
+# cp  $SCRIPT_PATH/libpyqt5_py2.so $LIB_PATH
+
+echo "copying libpyqt5_py3.9.so to $LIB_PATH"
+cp  $SCRIPT_PATH/libpyqt5_py3.9.so $LIB_PATH
