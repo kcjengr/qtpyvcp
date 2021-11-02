e7aac18cd3a83e1fe5f15f0240bc2ee530f37da8
Lukas <hardware.crash@gmail.com>
2021-11-02 15:13:59 +0000

    added switch for syntax highlighting to gcode_text_editor and qt designer property

--- qtpyvcp/widgets/input_widgets/gcode_text_edit.py
+++ qtpyvcp/widgets/input_widgets/gcode_text_edit.py
@@ -373,6 +373,14 @@ class GcodeTextEdit(QPlainTextEdit):
 
         super(GcodeTextEdit, self).changeEvent(event)
 
+    @Property(bool)
+    def syntaxHighlighting(self):
+        return self.syntax_highlighting
+    
+    @syntaxHighlighting.setter
+    def syntaxHighlighting(self, state):
+        self.syntax_highlighting = state
+
     def setPlainText(self, p_str):
         # FixMe: Keep a reference to old QTextDocuments form previously loaded
         # files. This is needed to prevent garbage collection which results in a
@@ -383,13 +391,14 @@ class GcodeTextEdit(QPlainTextEdit):
         doc.setDocumentLayout(QPlainTextDocumentLayout(doc))
         doc.setPlainText(p_str)
 
-        # start syntax heightening
-        self.gCodeHighlighter = GcodeSyntaxHighlighter(doc, self.font)
-
+        # start syntax highlighting
+        if self.syntax_highlighting == True:
+            self.gCodeHighlighter = GcodeSyntaxHighlighter(doc, self.font)
+        
         self.setDocument(doc)
         self.margin.updateWidth()
 
-        # start syntax heightening
+        # start syntax highlighting
         # self.gCodeHighlighter = GcodeSyntaxHighlighter(self)
 
     @Slot(bool)
