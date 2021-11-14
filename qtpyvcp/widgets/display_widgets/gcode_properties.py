bfc28435f3b63556eff090d217e2c036c4217ed7
TurBoss <j.l.toledano.l@gmail.com>
2021-11-14 05:06:46 +0100

    New widget, gcode properties
    shows some stats about the gcode program

---
+++ qtpyvcp/widgets/display_widgets/gcode_properties.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+
+from qtpy import uic
+from qtpy.QtWidgets import QWidget
+
+from qtpyvcp.widgets import VCPWidget
+from qtpyvcp.utilities import logger
+
+LOG = logger.getLogger(__name__)
+WIDGET_PATH = os.path.dirname(os.path.abspath(__file__))
+
+class GCodeProperties(QWidget):
+
+    def __init__(self, parent=None, standalone=False):
+        super(GCodeProperties, self).__init__(parent)
+
+        self.ui = uic.loadUi(os.path.join(WIDGET_PATH, "gcode_properties.ui"), self)
