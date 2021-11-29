c6e8f0a189f9f2f25c9a90f102be8fca6ff86925
TurBoss <j.l.toledano.l@gmail.com>
2021-11-29 23:32:34 +0100

    Qcompile now PySide2

--- qtpyvcp/tools/qcompile.py
+++ qtpyvcp/tools/qcompile.py
@@ -20,8 +20,8 @@ import sys
 import fnmatch
 import subprocess
 
-pyrcc = 'pyrcc5'
-pyuic = 'pyuic5'
+pyrcc = 'pyside2-rcc'
+pyuic = 'pyside2-uic'
 
 ok = "\033[32mok\033[0m"
 error = "\033[31mERROR - unable to call {}\033[0m"
@@ -31,7 +31,7 @@ def compile(packages=['.',]):
     for package in packages:
         package_path = os.path.abspath(package)
         if not os.path.isdir(package_path):
-            raise ValueError('Package "{}" not found!'.format(package))
+            raise ValueError(f'Package "{package}" not found!')
 
         # Compile Qt UI files
 
@@ -40,15 +40,12 @@ def compile(packages=['.',]):
             for f in files if f.endswith('.ui')]
 
         if len(files) > 0:
-            print(("Compiling .ui files in package '{}':".format(package)))
+            print(f"Compiling .ui files in package '{package}':")
 
             for infile in files:
                 outfile = infile.replace('.ui', '_ui.py')
 
-                sys.stdout.write("  {} => {} ... "
-                                 .format(os.path.basename(infile),
-                                         os.path.basename(outfile))
-                                 )
+                sys.stdout.write(f"\t{os.path.basename(infile)} => {os.path.basename(outfile)} ... ")
                 sys.stdout.flush()
 
                 try:
@@ -65,15 +62,12 @@ def compile(packages=['.',]):
                  for f in files if f.endswith('.qrc')]
 
         if len(files) > 0:
-            print(("\nCompiling .qrc files in package '{}':".format(package)))
+            print(f"\nCompiling .qrc files in package '{package}':")
 
             for infile in files:
                 outfile = infile.replace('.qrc', '_rc.py')
 
-                sys.stdout.write("  {} => {} ... " \
-                                 .format(os.path.basename(infile),
-                                         os.path.basename(outfile))
-                                 )
+                sys.stdout.write(f"\t{os.path.basename(infile)} => {os.path.basename(outfile)} ... ")
                 sys.stdout.flush()
 
                 try:
