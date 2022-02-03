2dc8f615d8ba7b9210503528aa6c5fd2b7069b9d
TurBoss <j.l.toledano.l@gmail.com>
2022-02-03 08:46:06 +0100

    FIX: Gcode text editor ( pixels cant be float )

--- qtpyvcp/widgets/input_widgets/gcode_text_edit.py
+++ qtpyvcp/widgets/input_widgets/gcode_text_edit.py
@@ -636,7 +636,7 @@ class NumberMargin(QWidget):
                 painter.setPen(self.color)
                 background = self.background
 
-            text_rec = QRect(0, block_top, self.width(), self.parent.fontMetrics().height())
+            text_rec = QRect(0, int(block_top), self.width(), self.parent.fontMetrics().height())
             painter.fillRect(text_rec, background)
             painter.drawText(text_rec, Qt.AlignRight, str(block_num + 1))
             block = block.next()
