1d149333eb5268f793a4a59506b408fec7488e2c
TurBoss <j.l.toledano.l@gmail.com>
2022-05-01 05:58:04 +0200

    test program extetens

--- qtpyvcp/widgets/display_widgets/vtk_backplot/program_bounds_actor.py
+++ qtpyvcp/widgets/display_widgets/vtk_backplot/program_bounds_actor.py
@@ -8,7 +8,7 @@ LOG = logger.getLogger(__name__)
 IN_DESIGNER = os.getenv('DESIGNER', False)
 
 
-class ProgramBoundsActor(vtk.vtkCubeAxesActor):
+class ProgramBoundsActor(vtk.vtkCubeAxesActor2D):
     def __init__(self, camera, path_actor):
         super(ProgramBoundsActor, self).__init__()
 
@@ -22,76 +22,34 @@ class ProgramBoundsActor(vtk.vtkCubeAxesActor):
         for attr_name in dir(VTKBackPlot):
             attr_value = getattr(VTKBackPlot, attr_name)
             LOG.debug(attr_name, attr_value, callable(attr_value))
-
+        
         LOG.debug(dir(VTKBackPlot))
         testit = getattr(VTKBackPlot, '_enableProgramTicks')
         LOG.debug('enableProgramTicks {}'.format(testit))
         """
-
-        self.SetBounds(self.path_actor.GetBounds())
-
+        
         self.SetCamera(camera)
+        self.SetNumberOfLabels(3)
+        
+        x_min, x_max, y_min, y_max, z_min, z_max = self.path_actor.GetBounds()
+        
+        self.SetBounds(x_min, x_max, y_min, y_max, z_min, z_max)
+        self.SetUseRanges(1)
+        self.SetRanges(0, x_max - x_min, 0, y_max - y_min, 0, z_max - z_min)
 
-        self.SetXLabelFormat("%6.3f")
-        self.SetYLabelFormat("%6.3f")
-        self.SetZLabelFormat("%6.3f")
-
-        self.SetFlyModeToStaticEdges()
-
-        self.GetTitleTextProperty(0).SetColor(1.0, 0.0, 0.0)
-        self.GetLabelTextProperty(0).SetColor(1.0, 0.0, 0.0)
-
-        self.GetTitleTextProperty(1).SetColor(0.0, 1.0, 0.0)
-        self.GetLabelTextProperty(1).SetColor(0.0, 1.0, 0.0)
-
-        self.GetTitleTextProperty(2).SetColor(0.0, 0.0, 1.0)
-        self.GetLabelTextProperty(2).SetColor(0.0, 0.0, 1.0)
+        self.SetLabelFormat("%6.3f mm")
+        self.SetFlyModeToOuterEdges()
 
         if not IN_DESIGNER:
             bounds = getSetting('backplot.show-program-bounds')
             self.showProgramBounds(bounds and bounds.value)
 
-            ticks = getSetting('backplot.show-program-ticks')
-            self.showProgramTicks(ticks and ticks.value)
-
-            labels = getSetting('backplot.show-program-labels')
-            self.showProgramLabels(labels and labels.value)
-
-    def showProgramLabels(self, labels):
-        if labels:
-            self.XAxisLabelVisibilityOn()
-            self.YAxisLabelVisibilityOn()
-            self.ZAxisLabelVisibilityOn()
-        else:
-            self.XAxisLabelVisibilityOff()
-            self.YAxisLabelVisibilityOff()
-            self.ZAxisLabelVisibilityOff()
-
-    def toggleProgramLabels(self):
-        self.showProgramLabels(not self.GetXAxisLabelVisibility())
-
-    def showProgramTicks(self, ticks):
-        if ticks:
-            self.XAxisTickVisibilityOn()
-            self.YAxisTickVisibilityOn()
-            self.ZAxisTickVisibilityOn()
-        else:
-            self.XAxisTickVisibilityOff()
-            self.YAxisTickVisibilityOff()
-            self.ZAxisTickVisibilityOff()
-
-    def toggleProgramTicks(self):
-        self.showProgramTicks(not self.GetXAxisTickVisibility())
-
     def showProgramBounds(self, bounds):
         if bounds:
-            self.XAxisVisibilityOn()
-            self.YAxisVisibilityOn()
-            self.ZAxisVisibilityOn()
+            self.SetShowActualBounds(True)
         else:
-            self.XAxisVisibilityOff()
-            self.YAxisVisibilityOff()
-            self.ZAxisVisibilityOff()
+            self.SetShowActualBounds(False)
 
     def toggleProgramBounds(self):
-        self.showProgramBounds(not self.GetXAxisVisibility())
+        self.showProgramBounds(not self.GetShowActualBounds())
+
