e90021462344ee8500b74e0a899ca8f89b499894
TurBoss <j.l.toledano.l@gmail.com>
2024-11-09 01:13:37 +0100

    Tool database: add field entries with query as property

--- src/qtpyvcp/widgets/db_widgets/designer_plugins.py
+++ src/qtpyvcp/widgets/db_widgets/designer_plugins.py
@@ -2,22 +2,26 @@ from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
 from qtpyvcp.widgets.qtdesigner.designer_plugin import RulesEditorExtension
 
 
-from qtpyvcp.widgets.db_widgets.tool_fields import DBToolSTLField
-
-class DBToolSTLFieldPlugin(_DesignerPlugin):
-    def pluginClass(self):
-        return DBToolSTLField
-    
-    
+from qtpyvcp.widgets.db_widgets.tool_fields import DBFileField
 from qtpyvcp.widgets.db_widgets.tool_fields import DBTextField
+from qtpyvcp.widgets.db_widgets.tool_fields import DBCheckBoxField
+from qtpyvcp.widgets.db_widgets.tool_fields import DBIntField
+
+class DBFileFieldPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return DBFileField
+    
 
 class DBTextFieldPlugin(_DesignerPlugin):
     def pluginClass(self):
         return DBTextField
-    
-    
-from qtpyvcp.widgets.db_widgets.tool_fields import DBCheckBoxField
+
 
 class DBCheckBoxFieldPlugin(_DesignerPlugin):
     def pluginClass(self):
         return DBCheckBoxField
+
+
+class DBIntPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return DBIntField
--- src/qtpyvcp/widgets/db_widgets/tool_fields.py
+++ src/qtpyvcp/widgets/db_widgets/tool_fields.py
@@ -4,7 +4,18 @@ Tool Model Property fields
 """
 
 from qtpy.QtCore import Property, Slot
-from qtpy.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QPushButton, QFileDialog, QDialog, QLabel
+
+from qtpy.QtWidgets import (
+    QWidget,
+    QLineEdit,
+    QHBoxLayout,
+    QPushButton,
+    QFileDialog,
+    QDialog,
+    QLabel,
+    QCheckBox,
+    QSpinBox
+)
 
 
 from qtpyvcp.lib.db_tool.base import Session, Base, engine
@@ -13,16 +24,17 @@ from qtpyvcp.lib.db_tool.tool_table import ToolTable, Tool, ToolProperties
 from qtpyvcp.utilities.logger import getLogger
 from qtpyvcp.widgets.dialogs.base_dialog import BaseDialog
 from qtpyvcp.widgets.base_widgets.base_widget import VCPWidget
-from PyQt5.Qt import QCheckBox
+from blinker._utilities import text
 
 LOG = getLogger(__name__)
 
 
-class DBToolSTLField(QWidget):
+
+class DBFileField(QWidget):
     """Tool Model property Widget"""
 
     def __init__(self, parent=None):
-        super(DBToolSTLField, self).__init__(parent)
+        super(DBFileField, self).__init__(parent)
         
         self.session = Session()
         
@@ -37,7 +49,7 @@ class DBToolSTLField(QWidget):
         self.label = QLabel(self._label_text)
         self.model_path = QLineEdit()
         self.browse_button = QPushButton()
-        self.browse_button.setText("Load STL")
+        self.browse_button.setText("Load")
         self.browse_button.clicked.connect(self.stl_dialog)
         
         self.layout.addWidget(self.label)
@@ -64,23 +76,31 @@ class DBToolSTLField(QWidget):
 
     @Slot(int)
     def toolSelected(self, tool_no):
-        self.tool_selected = tool_no - 1
         
-        tool_data = self.session.query(ToolProperties).filter(ToolProperties.tool_no == self.tool_selected).first()
+        table, column = self._query.split('.')            
+        table_class = globals().get(table)
+        
+        self.tool_selected = tool_no
+        
+        tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
         
         if tool_data:
-            self.setFilename(tool_data.model)
+            val = getattr(tool_data, column, None)
+            self.setFilename(val)
         else:
             self.clearFilename()
     
     @Slot()
     def saveField(self):
         if self.tool_selected is not None:
-            
-            tool_data = self.session.query(ToolProperties).filter(ToolProperties.tool_no == self.tool_selected).first()
         
+            table, column = self._query.split('.')            
+            table_class = globals().get(table)
+            
+            tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
+            
             if tool_data:
-                tool_data.model = self.filename
+                setattr(tool_data, column, self.filename)
                 self.session.commit()
         
     def setFilename(self, name):
@@ -132,23 +152,30 @@ class DBTextField(QWidget):
 
     @Slot(int)
     def toolSelected(self, tool_no):
-        self.tool_selected = tool_no - 1
-        tool_data = self.session.query(Tool).filter(Tool.tool_no == self.tool_selected).first()
+        
+        table, column = self._query.split('.')            
+        table_class = globals().get(table)
+        
+        self.tool_selected = tool_no
+        tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
         
         if tool_data:
-            self.text.setText(tool_data.remark)
+            val = getattr(tool_data, column, None)
+            self.text.setText(val)
         else:
             self.text.setText("")
             
     @Slot()
     def saveField(self):
         if self.tool_selected is not None:
-            self.tool_selected = tool_no - 1
             
-            tool_data = self.session.query(Tool).filter(Tool.tool_no == self.tool_selected).first()
+            table, column = self._query.split('.')
+            table_class = globals().get(table)
+            
+            tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
         
             if tool_data:
-                tool_data.remark = self.text.text()
+                setattr(tool_data, column, self.text.text())
                 self.session.commit()
     
     @Property(str)
@@ -193,12 +220,18 @@ class DBCheckBoxField(QWidget):
 
     @Slot(int)
     def toolSelected(self, tool_no):
-        self.tool_selected = tool_no - 1
         
-        tool_data = self.session.query(ToolProperties).filter(ToolProperties.tool_no == self.tool_selected).first()
+        table, column = self._query.split('.')            
+        table_class = globals().get(table)
+        
+        self.tool_selected = tool_no
+        
+        
+        tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
         
         if tool_data:
-            self.checkbox.setCheckState(bool(int(tool_data.model)))
+            val = getattr(tool_data, column, None)
+            self.checkbox.setCheckState(bool(int(val)))
         else:
             self.checkbox.setCheckState(False)
             
@@ -206,10 +239,87 @@ class DBCheckBoxField(QWidget):
     def saveField(self):
         if self.tool_selected is not None:
             
-            tool_data = self.session.query(ToolProperties).filter(ToolProperties.tool_no == self.tool_selected).first()
+            table, column = self._query.split('.')
+            table_class = globals().get(table)
+            
+            tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
+        
+            
+            if tool_data:
+                setattr(tool_data, column, self.checkbox.isChecked())
+                self.session.commit()
+    
+    @Property(str)
+    def labelText(self):
+        return self._label_text
+
+    @labelText.setter
+    def labelText(self, text):
+        self.label.setText(text)
+        self._label_text = text
+
+    @Property(str)
+    def query(self):
+        return self._query
+
+    @query.setter
+    def query(self, text):
+        self._query = text
+
+
+class DBIntField(QWidget):
+    """Tool Integer property Widget"""
+
+    def __init__(self, parent=None):
+        super(DBIntField, self).__init__(parent)
+        
+        self.session = Session()
+        
+        self.tool_selected = None
+        
+        self.layout = QHBoxLayout(self)
+        self._label_text = None
+        self._query = None
+        
+        
+        self.label = QLabel()
+        self.spinbox = QSpinBox()
+        
+                
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.spinbox)
+        
+
+    @Slot(int)
+    def toolSelected(self, tool_no):
+        
+        table, column = self._query.split('.')            
+        table_class = globals().get(table)
+            
+        self.tool_selected = tool_no
+        
+        tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
+
+        
+        if tool_data:
+            val = getattr(tool_data, column, None)
+            self.spinbox.setValue(int(val))
+        else:
+            self.spinbox.setValue(0)
+            self.spinbox.setDisabled(True)
+            
+    @Slot()
+    def saveField(self):
+        
+        if self.tool_selected is not None:
+            
+            table, column = self._query.split('.')
+            table_class = globals().get(table)
+            
+            tool_data = self.session.query(table_class).filter(table_class.tool_no == self.tool_selected).first()
         
             if tool_data:
-                tool_data.atc = self.checkbox.isChecked()
+                setattr(tool_data, column, self.spinbox.value())
                 self.session.commit()
     
     @Property(str)
