8b6468d63ac653d108fdc768bdb1ecca5131046a
James <james@snaggingpixels.com>
2025-02-15 10:33:49 +1300

    Fixed typo on import line

--- src/qtpyvcp/widgets/db_widgets/register_database.py
+++ src/qtpyvcp/widgets/db_widgets/register_database.py
@@ -1,7 +1,7 @@
 #import pydevd; pydevd.settrace()
 from qtpyvcp.widgets.db_widgets.tool_model import (ToolSTLField)  # noqa: F401
 
-from qtpyvcp.widgets.button_widgets.designer_plugins import (ToolSTLFieldPlugin)
+from qtpyvcp.widgets.db_widgets.designer_plugins import (ToolSTLFieldPlugin)
 
 from PySide6.QtDesigner import QPyDesignerCustomWidgetCollection
 
