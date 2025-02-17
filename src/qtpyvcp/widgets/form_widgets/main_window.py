131796bedb0677211c91202b9e88b568557cfbe1
James <james@snaggingpixels.com>
2025-02-18 12:22:37 +1300

    Added in class found to on fly compile UI files to in memory python.

--- src/qtpyvcp/widgets/form_widgets/main_window.py
+++ src/qtpyvcp/widgets/form_widgets/main_window.py
@@ -23,6 +23,50 @@ LOG = logger.getLogger(__name__)
 INFO = Info()
 STATUS = getPlugin('status')
 
+import subprocess
+import xml.etree.ElementTree as xml
+class PySide6Ui:
+    """
+        class to load .ui files to memory or
+        convert them to .py files
+
+        based on:
+        http://stackoverflow.com/a/14195313/3781327
+
+        usage:
+        PySide6Ui('myUi.ui').toPy('myUi.py')
+        PySide6Ui('myUi.ui').toPy()
+
+        PySide6Ui('myUi.ui').load()
+    """
+    def __init__(self, ui_file):
+        self.__ui_file = ui_file
+
+    def __getUi(self):
+        return subprocess.check_output(['pyside6-uic', self.__ui_file])
+
+    def toPy(self, py_file=None):
+        py_file = py_file or self.__ui_file.replace('.ui','.py')
+        uipy = self.__getUi()
+        try:
+            # Write the file
+            with open(py_file, 'w') as f:
+                f.write(uipy.decode("utf-8"))
+            return True
+        except:
+            return False
+
+    def load(self):
+        uipy = self.__getUi()
+        parsed = xml.parse(self.__ui_file)
+        widget_class = parsed.find('widget').get('class')
+        form_class = parsed.find('class').text
+        pyc = compile(uipy, '<string>', 'exec')
+        frame = {}
+        exec(pyc, frame)
+        form_class = frame['Ui_%s'%form_class]
+        base_class = eval('%s'%widget_class)# 'QMainWindow' or 'QWidget'
+        return form_class, base_class
 
 class VCPMainWindow(QMainWindow):
 
@@ -47,7 +91,10 @@ class VCPMainWindow(QMainWindow):
         # Load the UI file AFTER defining variables, otherwise the values
         # set in QtDesigner get overridden by the default values
         if ui_file is not None:
-            self.loadUi(ui_file)
+            form_class, base_class = PySide6Ui(ui_file).load()
+            form = form_class()
+            form.setupUi(self)
+            #self.loadUi(ui_file)
             # self.initUi()
 
         if menu is not None:
