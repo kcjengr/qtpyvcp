02618c3655deb8875e47b9d4fa2789bdadde9fe1
James <james@snaggingpixels.com>
2025-02-19 09:51:36 +1300

    Split out pyside UI loader to utils

--- src/qtpyvcp/widgets/form_widgets/main_window.py
+++ src/qtpyvcp/widgets/form_widgets/main_window.py
@@ -18,55 +18,12 @@ from qtpyvcp.plugins import getPlugin
 from qtpyvcp.utilities.settings import getSetting
 from qtpyvcp.widgets.dialogs import showDialog as _showDialog
 from qtpyvcp.app.launcher import _initialize_object_from_dict
+from qtpyvcp.utilities.pyside_ui_loader import PySide6Ui
 
 LOG = logger.getLogger(__name__)
 INFO = Info()
 STATUS = getPlugin('status')
 
-import subprocess
-import xml.etree.ElementTree as xml
-class PySide6Ui:
-    """
-        class to load .ui files to memory or
-        convert them to .py files
-
-        based on:
-        http://stackoverflow.com/a/14195313/3781327
-
-        usage:
-        PySide6Ui('myUi.ui').toPy('myUi.py')
-        PySide6Ui('myUi.ui').toPy()
-
-        PySide6Ui('myUi.ui').load()
-    """
-    def __init__(self, ui_file):
-        self.__ui_file = ui_file
-
-    def __getUi(self):
-        return subprocess.check_output(['pyside6-uic', self.__ui_file])
-
-    def toPy(self, py_file=None):
-        py_file = py_file or self.__ui_file.replace('.ui','.py')
-        uipy = self.__getUi()
-        try:
-            # Write the file
-            with open(py_file, 'w') as f:
-                f.write(uipy.decode("utf-8"))
-            return True
-        except:
-            return False
-
-    def load(self):
-        uipy = self.__getUi()
-        parsed = xml.parse(self.__ui_file)
-        widget_class = parsed.find('widget').get('class')
-        form_class = parsed.find('class').text
-        pyc = compile(uipy, '<string>', 'exec')
-        frame = {}
-        exec(pyc, frame)
-        form_class = frame['Ui_%s'%form_class]
-        base_class = eval('%s'%widget_class)# 'QMainWindow' or 'QWidget'
-        return form_class, base_class
 
 class VCPMainWindow(QMainWindow):
 
