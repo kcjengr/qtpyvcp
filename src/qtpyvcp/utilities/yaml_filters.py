cdc4a9354e007ba3375326f59f1fdae79dc6e530
TurBoss <j.l.toledano.l@gmail.com>
2025-05-25 01:24:53 +0200

    Fix running Designer with the latest changes ( INI to YML )

--- src/qtpyvcp/utilities/yaml_filters.py
+++ src/qtpyvcp/utilities/yaml_filters.py
@@ -3,7 +3,13 @@ import configparser
 from pprint import pprint
 
 def from_ini():
-    ini_file = os.getenv("INI_FILE_NAME")
+    ini_file = os.getenv("INI_FILE_NAME", False)
+    if not ini_file:
+        return {'traj': {'coordinates': 'XYZ'},
+                'machine': {'name': 'My Machine'},
+                'display': {'cycle_time': 100},
+                }
+        
     config = configparser.ConfigParser(strict=False)
     config.read(ini_file)
     result = {}
