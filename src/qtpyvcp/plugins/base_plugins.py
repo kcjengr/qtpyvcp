24fad68beac94485064011c80f04d57f12a81d94
James Walker <james@snaggingpixels.com>
2026-05-17 16:56:55 +1200

    FIX: Getter missing return

--- src/qtpyvcp/plugins/base_plugins.py
+++ src/qtpyvcp/plugins/base_plugins.py
@@ -159,7 +159,7 @@ class DataChannel(QObject):
 
     def getter(self, fget):
         def inner(*args, **kwargs):
-            fget(*args, **kwargs)
+            return fget(*args, **kwargs)
 
         self.fget = inner
         return self
